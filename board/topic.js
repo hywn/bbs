bfd3d45820519ad0110771cc18656801c06c8dd3
hywn <jh7qbe@virginia.edu>
2020-08-16 14:59:21 -0400

    some adjustments

--- board/topic.js
+++ board/topic.js
@@ -1,11 +1,14 @@
-document.getElementById("button").addEventListener("click", myFunction);
-function myFunction(){
-  let temp = document.createElement('p');
-  temp.innerHTML = document.getElementById("name").value + ":<br>";
-  temp.innerHTML += document.getElementById("content").value;
-  temp.style = "font-family:courier; background-color: pink; padding: 10px";
-  let div = document.getElementById("div");
-  div.append(temp);
-  document.getElementById("name").value = "";
-  document.getElementById("content").value = "";
-}
+const SUBMIT_BUTTON = document.querySelector('#submit_button')
+const CONTENT_FIELD = document.querySelector('#content')
+
+SUBMIT_BUTTON.addEventListener('click', e => {
+	const temp = document.createElement('p');
+
+	temp.innerHTML = document.getElementById("name").value + ":<br>";
+	temp.innerHTML += CONTENT_FIELD.value;
+	temp.style = "font-family:courier; background-color: pink; padding: 10px";
+
+	document.getElementById("div").append(temp);
+
+	CONTENT_FIELD.value = ''
+})
